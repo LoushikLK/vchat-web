2fc8297c6de9059e461a8731e9eb560ef92aca27
Loushik <loushikkumargiri@gmail.com>
2022-11-20 22:04:23 +0530

    working on video stream

---
+++ src/components/chat/VideoChat.tsx
@@ -0,0 +1,41 @@
+import { Avatar, Button, Input } from "@chakra-ui/react";
+
+const VideoChat = () => {
+  return (
+    <div className="w-full relative min-h-screen bg-gray-900">
+      <h3 className="font-medium tracking-wide text-lg p-4 bg-blue-900 ">
+        Live Chat
+      </h3>
+      <div className="w-full flex flex-col-reverse  ">
+        <div className="w-full flex items-start gap-2 p-4 ">
+          <Avatar size={"sm"} />
+          <span className="flex flex-col gap-1  max-w-[70%] justify-end ">
+            <small className="tracking-wide bg-blue-500 rounded-r-full rounded-bl-full p-2">
+              wjbiwedbweid ewidbiuduedeud iewdwd d
+            </small>
+
+            <small className=" ml-4 text-xs text-blue-700 text-right ">
+              20:30 PM
+            </small>
+          </span>
+        </div>
+        <div className="w-full flex items-start justify-end  gap-2 p-4 ">
+          <span className="flex flex-col gap-1 max-w-[70%]  ">
+            <small className="tracking-wide bg-gray-200 text-gray-900 rounded-l-full rounded-br-full p-2">
+              wjbiwedbweid ewidbiuduedeud iewdwd d
+            </small>
+
+            <small className=" ml-4 text-xs  text-blue-700 ">20:30 PM</small>
+          </span>
+          <Avatar size={"sm"} />
+        </div>
+      </div>
+      <div className="absolute right-0 bottom-0 flex items-center gap-4 p-4 w-full bg-blue-900 ">
+        <Input placeholder="Type message..." />{" "}
+        <Button className="!bg-blue-500">Send</Button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoChat;
--- src/pages/video/Call.tsx
+++ src/pages/video/Call.tsx
@@ -6,41 +6,101 @@ import {
   People,
   VideoOutlined,
 } from "assets/Icons";
+import { VideoChat } from "components/chat";
+import { useEffect, useState } from "react";
 
 const CallUI = () => {
+  const [drawerActive, setDrawerActive] = useState(true);
+
+  const [videoStream, setVideoStream] = useState<any>(undefined);
+
+  // useEffect(() => {
+  //   let timeOut = setInterval(() => {
+  //     setDrawerActive(!drawerActive);
+  //   }, 1000);
+
+  //   return () => {
+  //     clearInterval(timeOut);
+  //   };
+  // }, [drawerActive]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (mounted) {
+      (async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+
+        console.log(stream);
+        setVideoStream(stream);
+      })();
+    }
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
   return (
-    <section className="w-full bg-gray-900 min-h-screen relative text-white  ">
-      <div className=" fixed top-1/2 left-1/2 bg-blue-500 h-60 rounded-full -translate-x-1/2 -translate-y-1/2 w-60 text-7xl text-center  flex items-center justify-center  ">
-        LK
-      </div>
-      <div className="w-fit fixed bottom-12 left-1/2 -translate-x-1/2 ">
-        <div className="flex items-center gap-4  p-4 bg-blue-500/50  rounded-md shadow-lg">
-          <Button>
-            <span>
-              <People className="text-gray-900 text-4xl p-2 " />
-            </span>
-          </Button>
-          <Button>
-            <span>
-              <ChatOutlined className="text-gray-900 p-2 text-4xl " />
-            </span>
-          </Button>
-          <Button>
-            <span>
-              <MicOutlined className="text-blue-500 p-2 text-4xl " />
-            </span>
-          </Button>
-          <Button>
-            <span>
-              <VideoOutlined className="text-blue-500 p-2 text-4xl " />
-            </span>
-          </Button>
-          <Button className="!bg-red-500">
-            <span className="flex items-center gap-4">
-              <h3 className="font-medium tracking-wide text-base">END CALL</h3>
-              <Call className="text-white text-3xl " />
-            </span>
-          </Button>
+    <section className="w-full  relative text-white  ">
+      <div className="w-full flex">
+        <div
+          className={` ${
+            drawerActive ? "w-[calc(100vw-500px)]" : "w-full"
+          } h-full min-h-screen  border-r relative transition-all ease-in-out duration-300  bg-gray-900 border-white `}
+        >
+          {/* <div className=" absolute top-1/2 left-1/2 bg-blue-500 h-60 rounded-full -translate-x-1/2 -translate-y-1/2 w-60 text-7xl text-center  flex items-center justify-center  ">
+            LK
+          </div> */}
+          <video
+            src={videoStream}
+            className="min-h-screen w-full"
+            autoPlay={true}
+          />
+        </div>
+
+        <div
+          className={` ${
+            drawerActive ? "w-[500px]" : "w-0"
+          } h-full min-h-screen  transition-all ease-in-out duration-300 `}
+        >
+          {" "}
+          <VideoChat />
+        </div>
+        <div className="w-fit z-50 fixed bottom-12 left-1/2 -translate-x-1/2 ">
+          <div className="flex items-center gap-4  p-4 bg-blue-500/50  rounded-md shadow-lg">
+            <Button>
+              <span>
+                <People className="text-gray-900 text-4xl p-2 " />
+              </span>
+            </Button>
+            <Button>
+              <span>
+                <ChatOutlined className="text-gray-900 p-2 text-4xl " />
+              </span>
+            </Button>
+            <Button>
+              <span>
+                <MicOutlined className="text-blue-500 p-2 text-4xl " />
+              </span>
+            </Button>
+            <Button>
+              <span>
+                <VideoOutlined className="text-blue-500 p-2 text-4xl " />
+              </span>
+            </Button>
+            <Button className="!bg-red-500">
+              <span className="flex items-center gap-4">
+                <h3 className="font-medium tracking-wide text-base">
+                  END CALL
+                </h3>
+                <Call className="text-white text-3xl " />
+              </span>
+            </Button>
+          </div>
         </div>
       </div>
     </section>
