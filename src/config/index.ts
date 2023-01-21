ebcc783916a70cbe3af60e152bccb6905b7bf240
Loushik <loushikkumargiri@gmail.com>
2023-01-21 17:52:44 +0530

    api changes added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://192.168.29.82:8000/api/v1/";
+// export const BASE_URL = "http://192.168.29.82:8000/api/v1/";
+export const BASE_URL = "https://vchat-server.onrender.com/api/v1/";
